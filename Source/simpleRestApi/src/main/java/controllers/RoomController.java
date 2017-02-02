4051814a1c00b2cf62f1bf3ac02739c5b56709ff
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2017-02-02 22:43:25 +0100

    Added same no content mechanism to number route, still have to do some tests (TDD would be great to use from now on)

--- Source/simpleRestApi/src/main/java/controllers/RoomController.java
+++ Source/simpleRestApi/src/main/java/controllers/RoomController.java
@@ -2,13 +2,11 @@ package controllers;
 
 
 import model.Room;
-import org.omg.CORBA.Request;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import repositories.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
@@ -29,30 +27,29 @@ public class RoomController {
         return roomRepository.findAll();
     }
 
-//    @RequestMapping(path = "/rooms/id/{id}", method = RequestMethod.GET, produces = "application/json")
-//    public @ResponseBody
-//    Room getRoomById(@PathVariable(value="id") String roomIdToSearch) {
-//        return roomRepository.findById(roomIdToSearch);
-//    }
+
     @RequestMapping(path = "/rooms/id/{id}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
     ResponseEntity<?> getRoomById(@PathVariable(value="id") String roomIdToSearch) {
         Room roomToReturn = roomRepository.findById(roomIdToSearch);
-        System.out.print("########### GETTING ROOM");
 
         if (roomToReturn == null) {
-            return new ResponseEntity<>("No room to return", HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
-            System.out.print("########### ROOM GOTTEN : " + roomToReturn);
             return new ResponseEntity<>(roomToReturn, HttpStatus.OK);
         }
-
     }
 
     @RequestMapping(path = "/rooms/number/{number}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
-    List<Room> getRoomByNumber(@PathVariable(value="number") Integer roomNumberToSearch) {
-        return roomRepository.findByNumber(roomNumberToSearch);
+    ResponseEntity<?> getRoomByNumber(@PathVariable(value="number") Integer roomNumberToSearch) {
+        List<Room> roomsToReturn = roomRepository.findByNumber(roomNumberToSearch);
+
+        if (roomsToReturn.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(roomsToReturn, HttpStatus.OK);
+        }
     }
 
 }
