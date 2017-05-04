e4ee69dfc0399ebfd6ecf5d84ec5775f4ad5e880
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2017-05-04 23:23:49 +0200

    Currently implementing swaggerui

--- src/main/java/controllers/GreetingsController.java
+++ src/main/java/controllers/GreetingsController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import io.swagger.annotations.ApiOperation;
 import model.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ public class GreetingsController {
   private static final String template = "Hello, %s";
   private final AtomicLong counter = new AtomicLong();
 
+  @ApiOperation(value = "getGreeting", nickname = "getGreeting")
   @RequestMapping(method=RequestMethod.GET, produces = "application/json")
   public @ResponseBody
   Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
--- src/main/java/model/Greeting.java
+++ src/main/java/model/Greeting.java
@@ -1,19 +1,24 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
 public class Greeting {
-  private final long id;
-  private final String content;
+    private final long id;
+    private final String content;
 
-  public Greeting(long id, String content) {
-    this.id = id;
-    this.content = content;
-  }
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
 
-  public long getId() {
-    return this.id;
-  }
+    public long getId() {
+        return this.id;
+    }
 
-  public String getContent() {
-    return this.content;
-  }
+    @JsonProperty(required = true)
+    @ApiModelProperty(notes = "The greetings content", required = true)
+    public String getContent() {
+        return this.content;
+    }
 }
