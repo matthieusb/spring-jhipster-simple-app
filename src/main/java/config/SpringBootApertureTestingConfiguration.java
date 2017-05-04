f797c7602e6e13057b7fe0c85ce8d0be9355feca
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2017-05-04 23:28:25 +0200

    Corrected component scan missing

--- src/main/java/config/SpringBootApertureTestingConfiguration.java
+++ src/main/java/config/SpringBootApertureTestingConfiguration.java
@@ -2,9 +2,11 @@ package config;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
