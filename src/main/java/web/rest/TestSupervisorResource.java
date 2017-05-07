74860b5e8c6ff22ae82c64baafb2d50003be4880
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2017-05-07 19:10:13 +0200

    Finished test supervisor refacto

--- src/main/java/web/rest/TestSupervisorResource.java
+++ src/main/java/web/rest/TestSupervisorResource.java
@@ -1,11 +1,14 @@
 package web.rest;
 
+import model.TestSupervisor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import repository.TestSupervisorRepository;
 import web.rest.util.ResponseEntityUtils;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/supervisors", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
@@ -20,22 +23,41 @@ public class TestSupervisorResource {//TODO REFACTO (See RoomResource)
 
     @GetMapping(produces = "application/json")
     public @ResponseBody
-    ResponseEntity<?> getAllTestSupervisors() {
-        return ResponseEntityUtils.
-                getResponseEntityForMultipleResponses(testSupervisorRepository.findAll());
+    ResponseEntity<List<TestSupervisor>> getAllTestSupervisors() {
+        List<TestSupervisor> testSupervisors = testSupervisorRepository.findAll();
+        if (testSupervisors.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok()
+                .headers(ResponseEntityUtils.getStandardHeaders())
+                .body(testSupervisors);
+        }
     }
 
     @GetMapping(path = "/id/{id}", produces = "application/json")
     public @ResponseBody
-    ResponseEntity<?> getTestSupervisorById(@PathVariable(value = "id") String testSupervisorIdToSearch) {
-        return ResponseEntityUtils.
-                getResponseEntityForSingleResponse(testSupervisorRepository.findById(testSupervisorIdToSearch));
+    ResponseEntity<TestSupervisor> getTestSupervisorById(@PathVariable(value = "id") String testSupervisorIdToSearch) {
+        TestSupervisor testSupervisor = testSupervisorRepository.findById(testSupervisorIdToSearch);
+        if (testSupervisor == null) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok()
+                .headers(ResponseEntityUtils.getStandardHeaders())
+                .body(testSupervisor);
+        }
+
     }
 
     @PostMapping(path = "/login", produces = "application/json")
     public @ResponseBody
-    ResponseEntity<?> getTestSupervisorByLogin(@RequestParam(value = "login") String testSupervisorLoginToSearch) {
-        return ResponseEntityUtils.
-                getResponseEntityForSingleResponse(testSupervisorRepository.findByLogin(testSupervisorLoginToSearch));
+    ResponseEntity<TestSupervisor> getTestSupervisorByLogin(@RequestParam(value = "login") String testSupervisorLoginToSearch) {
+        TestSupervisor testSupervisor = testSupervisorRepository.findByLogin(testSupervisorLoginToSearch);
+        if (testSupervisor == null) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok()
+                .headers(ResponseEntityUtils.getStandardHeaders())
+                .body(testSupervisor);
+        }
     }
 }
