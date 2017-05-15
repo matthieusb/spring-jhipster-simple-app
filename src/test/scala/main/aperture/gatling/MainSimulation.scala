ee1b342fd4594d9b37ba524f429cde5023ec9489
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2017-05-15 17:40:37 +0200

    Completed gatling scenario, enough for now

--- src/test/scala/main/aperture/gatling/MainSimulation.scala
+++ src/test/scala/main/aperture/gatling/MainSimulation.scala
@@ -12,9 +12,17 @@ class MainSimulation extends Simulation {
     val httpConf: HttpProtocolBuilder = http
         .baseURL("http://localhost:8080") // Here is the root for all relative URLs
 
-    val scn: ScenarioBuilder = scenario("Sample")
-        .exec(http("request1")
+    val scenario_main_get: ScenarioBuilder = scenario("Testing main get routes")
+        .exec(http("Request on main route")
             .get("/"))
         .pause(100 milliseconds)
-    setUp(scn.inject(rampUsers(20) over (5 seconds)).protocols(httpConf))
+        .exec(http("Request on rooms main route")
+            .get("/api/rooms"))
+        .pause(100 milliseconds)
+        .exec(http("Request on subjects main route")
+            .get("/api/subjects"))
+        .pause(100 milliseconds)
+        .exec(http("Request on supervisors main route")
+            .get("/api/supervisors"))
+    setUp(scenario_main_get.inject(rampUsers(1000) over (5 seconds)).protocols(httpConf))
 }
