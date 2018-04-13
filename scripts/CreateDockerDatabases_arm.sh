cf9a2d30199cbd6141eefb2f29cb4d0ff6c478e6
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2018-04-13 20:46:58 +0000

    Added arm docker config

---
+++ scripts/CreateDockerDatabases_arm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Name : CreateDockerDatabases.sh
+# Author : Matthieu Sauboua-Beneluz
+# Description : Create the mongodb container to use with this app for testing
+
+
+### To connect manually, install mongo on your host
+
+###############################
+#### MONGODB
+###############################
+
+# -- Stopping any mongo local service that has nothing to do with docker
+sudo /etc/init.d/mongodb stop
+
+# -- Creating and launching container
+docker run -p 27017:27017 --name spring_mongo_instance_01 -d mangoraft/mongodb-arm
+
+# -- Identification (Command line)
+# mongo --port 27017
